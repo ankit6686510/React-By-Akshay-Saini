88e189132d200fae2cc4890b543602eb4469bc7b
ankit <ankit6686510@gmmail.com>
2024-03-08 02:45:57 +0530

    completed iginiting our app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 /* <div>
     <div>
         <h1>i am h1</h1>
@@ -10,7 +12,7 @@ const parent = React.createElement(
     "div",
     { id: "parent" },
       React.createElement("div", { id: "child" }, [
-      React.createElement("h1",{}, "i am h1 tag"),
+      React.createElement("h1",{}, "namaste recat"),
       React.createElement("h2", {}, "i am h2 tag"),
     ])
   );
---
+++ notes.md
@@ -0,0 +1,2 @@
+## there are two dependeinceis aor package oin eis dev dependencies and one is  normal dependcies dev is used whole devloing dev app and normal is used in production 
+// 
